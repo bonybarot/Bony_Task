c6d35fe7d137356e2459bc8f5daef334ecc053c7
bonybarot <barotbony29@gmail.com>
2024-08-15 18:04:19 +0530

    renamed app

--- ios/Tabs_AppTests/Tabs_AppTests.m
+++ ios/Tabs_AppTests/Tabs_AppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Tabs_AppTests : XCTestCase
+@interface Task_BonyTests : XCTestCase
 
 @end
 
-@implementation Tabs_AppTests
+@implementation Task_BonyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
